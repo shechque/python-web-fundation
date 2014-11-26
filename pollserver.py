8fd82b3e76c1e5a6865620ab78893c65f098a9cf
shechque <shechque@gmail.com>
2014-11-26 14:18:54 +0800

    littleserver

---
+++ pollserver.py
@@ -0,0 +1,32 @@
+##simple server using poll
+
+import socket,select
+
+s=socket.socket()
+
+#host=socket.gethostname()
+host='127.0.0.1'
+port=1234
+s.bind((host,port))
+
+fdmap = {s.fileno(): s}
+s.listen(5)
+p = select.poll()
+p.register(s)
+while True:
+	events = p.poll()
+	for fd,event in events:
+		if fd in fdmap:
+			c,addr = s.accept()
+			print 'Got connection from',addr
+			p.register(c)
+			fdmap[c.fileno()] =c
+		elif event $ select.POLLIN:
+			data = fdmap[fd].recv(1024)
+			if not data:
+				print fdmap[fd].getpeername(),'disconnected'
+				p.unregister(fd)
+				del fdmap[fd]
+			else:
+				print data
+
